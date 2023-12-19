d60be5266c0f9485d126afd04a2c43fccbccfce0
casio <charlycop@free.fr>
2023-12-20 05:01:02 +0700

    first version, checking inside the last block, and displaying quantity of transactions within the block and display the sender, receiver and the amont

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Alchemy, Network } from 'alchemy-sdk';
 import { useEffect, useState } from 'react';
 
+// ...
+
 import './App.css';
 
 // Refer to the README doc for more information about using API
@@ -24,16 +26,58 @@ function App() {
   const [blockWithTransactions, setBlockWithTransactions] = useState();
 
   useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+    async function fetchData() {
+      try {
+        // Fetch the block number
+        const currentBlockNumber = await alchemy.core.getBlockNumber();
+        setBlockNumber(currentBlockNumber);
+  
+        // Use the block number to make another query
+        const blockWithTransactions_ = await alchemy.core.getBlockWithTransactions(currentBlockNumber);
+        setBlockWithTransactions(blockWithTransactions_);
+        
+        // Other logic or state updates can be added here
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // Handle errors as needed
+      }
     }
+  
+    fetchData();
 
-    async function getBlockWithTransactions() {
-      setBlockWithTransactions(await alchemy.core.getBlockWithTransactions(blockNumber));
-    }
-  });
+  }, []);
 
-  return <div className="App">Block Number: {blockNumber} {blockWithTransactions}</div>;
+
+  if (blockWithTransactions) {
+    return (
+      <div className="App">
+        <p>Block Number: {blockNumber}</p>
+  
+        <p>{blockWithTransactions.transactions.length} Transactions:</p>
+  
+        <div>
+          {blockWithTransactions.transactions.map((transaction, index) => (
+            <div key={index}>
+              <p>
+
+              <a href={`https://sepolia.etherscan.io/tx/${transaction.hash}`} target="_blank" rel="noopener noreferrer">Transaction[{transaction.transactionIndex}]</a><br />
+                From: <a href={`https://sepolia.etherscan.io/address/${transaction.from}`} target="_blank" rel="noopener noreferrer">{transaction.from}</a><br />
+                To: <a href={`https://sepolia.etherscan.io/address/${transaction.to}`} target="_blank" rel="noopener noreferrer">{transaction.to}</a><br />
+                Amount: {parseInt(transaction.value._hex, 16) / Math.pow(10, 9)} GWei<br />
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+  
+  else{
+    return <div className="App"></div>;
+  }
+  
+  
+  
 }
 
 export default App;
